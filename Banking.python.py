65e98b5ad59566f0d2c155d5f92ae020055b7fb1
swarupgund <79126668+swarupgund@users.noreply.github.com>
2021-03-07 16:09:19 +0530

    Update Banking.python.py

--- Banking.python.py
+++ Banking.python.py
@@ -2,19 +2,19 @@ class Bank_Account:
     def __init__(self):
         self.balance=0
         print("Welcome to the deposite and withdrawal machine")
-    def deposit(self):
-        amount=float(input("Enter amount to be deposite:"))
+    def Deposit(self):
+        amount=float(input("Enter amount to be Deposite:"))
         self.balance += amount
         print("Amount Deposited:",amount)
-    def withdraw(self):
-        amount=float(input("Enter amount to withdrawn:"))
+    def Withdraw(self):
+        amount=float(input("Enter amount to Withdrawn:"))
         if self.balance>=amount:
             self.balance-= amount
             print("You Withdrew:",amount)
         else:
             print("Insufficient balance.")
-    def display(self):
-        print("Net available balance:",self.balance)
+    def Display(self):
+        print("Net available Balance:",self.balance)
 s =Bank_Account()
 s.Deposit()
 s.Withdraw()
